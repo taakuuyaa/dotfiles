91632a1427b219432c8c276517ee4149eee76eb3
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2020-04-28 00:49:12 +0900

    comment out

--- install.zsh
+++ install.zsh
@@ -11,7 +11,8 @@ source $DOTDIR/lib_sh/requires.zsh
 
 bot "Hi! I'm going to install tooling and tweak your system settings. Here I go..."
 
-source $DOTDIR/osx/index.zsh
+# 設定中
+#source $DOTDIR/osx/index.zsh
 source $DOTDIR/brew/index.zsh
 source $DOTDIR/zsh/index.zsh
 
