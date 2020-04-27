72a9c6d6dfad836f61497988d06dec32755be0cf
taakuuyaa <51745522+taakuuyaa@users.noreply.github.com>
2020-04-27 20:23:47 +0900

    rename zprezto

--- zsh/zpreztorc.zsh
+++ zsh/zpreztorc.zsh
@@ -2,7 +2,7 @@ git submodule update --init --recursive
 
 # prezto
 setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+for rcfile in "${ZDOTDIR:-$HOME}"/zprezto/runcoms/^README.md(.N); do
  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
